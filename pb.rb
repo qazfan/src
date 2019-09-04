1cd76cacaafcb09770e18c17deb66b623ca09ae6
C Nees <cnees@umich.edu>
2019-09-03 20:19:29 -0700

    Add script for downloading watermark-free images

---
+++ pb.rb
@@ -0,0 +1,31 @@
+#! /usr/bin/ruby
+
+require 'open-uri'
+require 'fileutils'
+
+petpage_input_file = File.open(ARGV[0], 'r')
+petpage = petpage_input_file.read
+
+photobucket_urls = petpage.
+  scan(/http[s]?:\/\/[a-zA-Z0-9]*\.photobucket\.com[^><\"\')(]*\.(?:gif|jpg|png|jpeg|GIF|JPG|PNG|JPEG)/).
+  uniq
+
+puts photobucket_urls.join("\n")
+puts "Are all the URLs photobucket links? y/N"
+go = $stdin.gets
+exit unless go.chomp.downcase == 'y'
+
+petname = File.basename(petpage_input_file).split('.').first
+
+photobucket_urls.each do |photobucket_url|
+  path = "./#{petname}#{URI.parse(photobucket_url).path}"
+  puts "PATH: #{path}"
+  FileUtils.mkpath(path.split('/').first(path.split('/').size - 1).join('/'))
+  cmd = "curl -o #{path} --referer \"http://s.photobucket.com/\" #{photobucket_url}"
+  puts cmd
+  x = `#{cmd}`
+  puts x
+end
+
+
+# petpage.gsub(/http[s]?:\/\/[a-zA-Z0-9]*\.photobucket\.com/, )
